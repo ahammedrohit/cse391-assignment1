e2a882438f72d2e005bfc87b9bf4671aa66745e1
ahammedrohit <ahammedrohit@gmail.com>
2021-02-23 15:26:47 +0600

    Changed ID to Class and Added last Modified Time.

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var lastModified = document.lastModified;
+
+let lastModifiedText = "Last Modified : "+lastModified; 
+
+
+document.getElementById("lastMod").textContent = lastModifiedText;
+
+var str = "CSE391 Assignment 1";
+
+console.log("Thanks for Visiting the page. I made this for "+str); 
+console.log("Assignment Link");
+console.log("https://drive.google.com/file/d/1rE1uVJRS6sWQsGukfDU8nr6vLEEMDF_s/view?usp=sharing");
